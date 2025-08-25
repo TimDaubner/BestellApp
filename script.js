5c824bbc6684c72bfb6e94c2bd984cb1240b959d
Tim <tim.daubner@outlook.com>
2025-08-25 16:12:14 +0200

    commit design completed

--- script.js
+++ script.js
@@ -1,3 +1,62 @@
-function DOTHIS() {
-    console.log(`${Surename} leerzeichen ${name}`);
+let shoppingCart = [];
+
+function init() {
+    createIDsForFood();
+    renderAllItemsOnMenu();
+}
+
+function createIDsForFood() {
+    for (let i = 0; i < food.length; i++) {
+        food[i].id = i;
+    }
+}
+
+function getInputValue(id) {
+    return document.getElementById(id).value;
+}
+
+function getHTMLForFood(type) {
+    // switch
+}
+
+function renderAllItemsOnMenu() {
+    for (let i = 0; i < food.length; i++) {
+        let foodItems = document.getElementById(`all_order_items_${food[i].type}`);
+        foodItems.innerHTML = '';
+    }
+    for (let i = 0; i < food.length; i++) {
+        let foodItems = document.getElementById(`all_order_items_${food[i].type}`);
+        foodItems.innerHTML += getHTMLForMenuItem(i);
+    }
+}
+
+function renderShoppingCart(item) {
+    shoppingCart.push(food[item]);
+    document.getElementById('order_list').innerHTML = '';
+    for (let i = 0; i < shoppingCart.length; i++) {
+        document.getElementById('order_list').innerHTML += getHTMLForOrderListItem(i);
+    }
+}
+
+function getHTMLForMenuItem(i) {
+    return `
+            <div class="item-frame">
+                <li onclick="renderShoppingCart(${food[i].id})" class="item-content">
+                    ${food[i].name} ${food[i].price[1]} €
+                </li>
+                <img class="add-plus" src="./img/plus.svg" alt="add plus">
+            </div>`
+}
+
+function getHTMLForOrderListItem(i) {
+    return `
+    <p class="ordered-item"><b class="ordered-number">1 x</b> ${shoppingCart[i].name} <b class="price-tag">${shoppingCart[i].price[1]} €</b> <img class="trashcan" src="./img/trashcan.svg" alt="trashcan"></b> <imgclass="plus-ordered-number" src="./img/plus.svg"></p>`
+}
+
+function toggleFoodType(_type) {
+    let type = document.getElementById(`all_order_items_${_type}`);
+    document.getElementById('all_order_items_pizza').classList.add('d_none');
+    document.getElementById('all_order_items_salad').classList.add('d_none');
+    document.getElementById('all_order_items_dessert').classList.add('d_none');
+    type.classList.remove('d_none');
 }
---
+++ scripts/db.js
@@ -0,0 +1,128 @@
+let food = [
+    {
+        "type": "pizza",
+        "name": "Pizza Margherita",
+        "price": ["9.80", "10.30", "12.00"],
+        "size": ["small", "normal", "big"]
+    },
+    {
+        "type": "pizza",
+        "name": "Pizza Salami",
+        "price": ["9.90", "11.30", "12.10"],
+        "size": ["small", "normal", "big"]
+    },
+    {
+        "type": "pizza",
+        "name": "Pizza Funghi",
+        "price": ["10.50", "11.30", "12.00"],
+        "size": ["small", "normal", "big"]
+    },
+    {
+        "type": "pizza",
+        "name": "Pizza Frutti di Marè",
+        "price": ["12.00", "12.50", "13.20"],
+        "size": ["small", "normal", "big"]
+    },
+    {
+        "type": "pizza",
+        "name": "Pizza Bolo",
+        "price": ["13.00", "14.00", "15.00"],
+        "size": ["small", "normal", "big"]
+    },
+    {
+        "type": "pizza",
+        "name": "Pizza T-Rex",
+        "price": ["13.80", "15.20", "16.00"],
+        "size": ["small", "normal", "big"]
+    },
+    {
+        "type": "pizza",
+        "name": "Pizza Speciale",
+        "price": ["13.80", "14.30", "15.10"],
+        "size": ["small", "normal", "big"]
+    },
+    {
+        "type": "salad",
+        "name": "Salat Gemischt",
+        "price": ["9.80", "10.30", "12.00"],
+        "size": ["small", "normal", "big"]
+    },
+    {
+        "type": "salad",
+        "name": "Salat Tomate Mozzarella",
+        "price": ["9.90", "11.30", "12.10"],
+        "size": ["small", "normal", "big"]
+    },
+    {
+        "type": "salad",
+        "name": "Salat Tonno",
+        "price": ["10.50", "11.30", "12.00"],
+        "size": ["small", "normal", "big"]
+    },
+    {
+        "type": "salad",
+        "name": "Salat Frutti di Marè",
+        "price": ["12.00", "12.50", "13.20"],
+        "size": ["small", "normal", "big"]
+    },
+    {
+        "type": "salad",
+        "name": "Salat Tri Colori",
+        "price": ["13.00", "14.00", "15.00"],
+        "size": ["small", "normal", "big"]
+    },
+    {
+        "type": "salad",
+        "name": "Salat Grün",
+        "price": ["13.80", "15.20", "16.00"],
+        "size": ["small", "normal", "big"]
+    },
+    {
+        "type": "salad",
+        "name": "Salat Olive",
+        "price": ["13.80", "14.30", "15.10"],
+        "size": ["small", "normal", "big"]
+    },
+    {
+        "type": "dessert",
+        "name": "Tiramisu",
+        "price": ["9.80", "10.30", "12.00"],
+        "size": ["small", "normal", "big"]
+    },
+    {
+        "type": "dessert",
+        "name": "Panna Cotta",
+        "price": ["9.90", "11.30", "12.10"],
+        "size": ["small", "normal", "big"]
+    },
+    {
+        "type": "dessert",
+        "name": "Creme della Lux",
+        "price": ["10.50", "11.30", "12.00"],
+        "size": ["small", "normal", "big"]
+    },
+    {
+        "type": "dessert",
+        "name": "Dino Cake",
+        "price": ["12.00", "12.50", "13.20"],
+        "size": ["small", "normal", "big"]
+    },
+    {
+        "type": "dessert",
+        "name": "Dino Waffeln",
+        "price": ["13.00", "14.00", "15.00"],
+        "size": ["small", "normal", "big"]
+    },
+    {
+        "type": "dessert",
+        "name": "Dino Milkshake",
+        "price": ["13.80", "15.20", "16.00"],
+        "size": ["small", "normal", "big"]
+    },
+    {
+        "type": "dessert",
+        "name": "Snack Platte",
+        "price": ["13.80", "14.30", "15.10"],
+        "size": ["small", "normal", "big"]
+    }
+]
